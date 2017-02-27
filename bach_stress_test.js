19ade71f04c7f745d2ecec682cfeb28292c8c991
giop <giovanni.bedetti@gmail.com>
2017-02-27 17:35:05 +0100

    code cleaning and css styling
    removing unnecessary code that was running until images are loaded. now
    images are loaded before setup() starts.
    canvas resizes to fill available space.
    some css styling to have the canvas centered.
    project description added in html

--- bach_stress_test.js
+++ bach_stress_test.js
@@ -1,5 +1,3 @@
-
-
 /*
 
  bach stress test
@@ -24,7 +22,7 @@
  Check out the magic of math.
  
  Desktop only: 
- for your pleasure you can stop the sounds using the space bar, 
+ for your pleasure you can stop the sounds pressing 'p', 
  and enable/disable the random image change pressing 's'.
  
  Warning: as I said, this is a stress test. Try it at your own risk! (your browser can hang a little bit)
@@ -40,55 +38,53 @@ var allRects;
 var imgCount = 8;
 var images = [imgCount];
 // Keeps track of loaded images (true or false)
-var loadStates = [imgCount]; 
+var loadStates = [imgCount]; //new boolean[imgCount];
 // For loading animation
 var loaderX, loaderY, theta;
 var still = false;
 //how many divisions are done
 var times = 0;
+//the div containing this canvas
+var sketchParent;
+
+function preload(){
+  loadImages();
+}
+
+function windowResized() {
+  resizeCanvas(sketchParent.offsetWidth, sketchParent.offsetHeight);
+  stopSound(allRects.length);
+  initRects();
+  times = 0;
+  still = false;
+  createBuffers(1);
+  playAtIndex(0);
+}
 
 function setup() {
   initAudio();
-
-  createCanvas(900, 514);
-  loadImages();
+  sketchParent = document.getElementById("sketch-holder");
+  var canvas = createCanvas(sketchParent.offsetWidth, sketchParent.offsetHeight);
+  canvas.parent('sketch-holder');
 
   initRects();
-  
   textSize(12);
+  canvas.mousePressed(onMousePressed);
 }
 
 function draw() {
-  if (checkLoadStates()) background(255);
-  else {
-    fill(0, 20);
-    rect(0, 0, width, height);
-  }
-
-  // Start loading animation
-  runLoaderAni();
-  //check if images are loaded
-   updateLoadStates();
-
-  // When all images are loaded draw them to the screen
-  if (checkLoadStates()) {
-    for (var i = 0; i < allRects.length; i++) {
-      allRects[i].display();
+  background(255);
+  for (var i = 0; i < allRects.length; i++) {
+        allRects[i].display();
     }
-  }
-    fill(0);
-    rect(0, height-20, width, height);
-    
-    fill(255, 0, 0);
-    text(allRects.length+" buffer(s) playing, Processing running at "+nf(frameRate(), 2, 2)+" fps", width- 300, height-5);
-  
+  //draw some info about the test
+  fill(0);
+  rect(0, height-20, width, height);
+  fill(255, 0, 0);
+  text(allRects.length+" buffer(s) playing, Processing running at "+nf(frameRate(), 2, 2)+" fps", width-300, height-5);  
 }
 
-
-
-
-function mousePressed() {
-  
+function onMousePressed() {/*mobile chrome fires mousePressed twice, wait for p5.js fix*/
   if (times >= 0) {
     still = true;
   }
@@ -104,41 +100,34 @@ function mousePressed() {
     for (var i=0; i < allRects.length; i++) {
       playAtIndex(i);
     }
-  } else 
-  {
+  } else {
     background(255);
     stopSound(allRects.length);
-
     initRects();
     times = 0;
     still = false;
-
     createBuffers(1);
     playAtIndex(0);
   }
 }
 
 function keyPressed() {
-  if (key == 's'|| key== 'S') {
+  if (key=='s'||key=='S') {
     still = !still;
-    
     for (var i = 0; i < allRects.length; i++) {
       allRects[i].setStill(still);
     }
   }
-
-  if (key == ' ') {
+  if (key=='p'||key=='P') {
     stopSound(allRects.length);
   }
 }
-
 
 function divideRects() {
   var tempRects = [4];
   var newRects = [allRects.length*4];
 
   for (var i = 0; i < allRects.length; i++) {
-
     tempRects = allRects[i].divide();
     //copy tempRects to newRects
     for (var j = 0; j < tempRects.length; j++) {
@@ -149,45 +138,13 @@ function divideRects() {
 }  
 
 function loadImages() {
-  for (var i = 0; i < 8; i++) { 
+  for (var i=0; i< 8; i++) { 
+    loadStates[i] = false;
     images[i] = loadImage("data/canone_V2_0"+i+".png");
   }
 }
 
 function initRects() {
   allRects = [1];
-  allRects[0]=new DrawingRect(images, 0, 0, width, height-20);
-}
-
-// Loading animation
-function runLoaderAni() {
-  // Only run when images are loading
-  if (!checkLoadStates()) {
-    fill(255);
-    ellipse(loaderX, loaderY, 10, 10);
-    loaderX = width/2 + cos(theta) * width/32;
-    loaderY = height/2 + sin(theta) * (height/16);
-    theta += PI/22;
-  }
-}
-
-// Return true when all images are loaded - no false values left in array 
-function checkLoadStates() {
-  for (var i = 0; i < images.length; i++) {
-    if (loadStates[i] == false) {
-      return false;
-    }
-  }
-  
-  return true;
-}
-
-function updateLoadStates(){
-  for (var i = 0; i < images.length; i++){
-    // Check if individual images are fully loaded
-    if ((images[i].width != 0) && (images[i].width != -1)){
-      // As images are loaded set true in boolean array
-      loadStates[i] = true;
-    }
-  }
+  allRects[0] = new DrawingRect(images, 0, 0, width, height-20);
 }
